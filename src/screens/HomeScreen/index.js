5228a9b35f2da5135355f11be4a9e37e8f88033c
Rokas <rookas.rudzenskas@gmail.com>
2021-07-28 16:39:57 +0300

    Rokas

--- src/screens/HomeScreen/index.js
+++ src/screens/HomeScreen/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Text, View} from "react-native";
+import styles from "./styles";
 
 const HomeScreen = () => {
     return (
-        <View>
+        <View style={styles.container}>
             <Text>Rokas</Text>
         </View>
     );
