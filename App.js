6b3d82299278f6c9c47cd13c178118bbc7170174
Rokas <rookas.rudzenskas@gmail.com>
2021-07-28 16:36:15 +0300

    Rokas

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Uber App</Text>
-      <StatusBar style="auto" />
-    </View>
+      <SafeAreaView>
+        <View style={styles.container}>
+          <Text>Uber App</Text>
+          <StatusBar style="auto" />
+        </View>
+      </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
 });
