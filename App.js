52abfda48a5b2e4575a4273f69793b14c6a8a9dd
Rokas <rookas.rudzenskas@gmail.com>
2021-07-28 16:38:39 +0300

    Rokas

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import HomeScreen from "./src/screens/HomeScreen";
 
 export default function App() {
   return (
       <SafeAreaView>
         <View style={styles.container}>
-          <Text>Uber App</Text>
+          <HomeScreen />
           <StatusBar style="auto" />
         </View>
       </SafeAreaView>
---
+++ src/screens/HomeScreen/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+const HomeScreen = () => {
+    return (
+        <View>
+            <Text>Rokas</Text>
+        </View>
+    );
+};
+
+export default HomeScreen;
