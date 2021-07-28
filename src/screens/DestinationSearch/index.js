272bf83eb99bfd7f0241ee207a955c74829715ee
Rokas <rookas.rudzenskas@gmail.com>
2021-07-28 18:30:20 +0300

    Rokas

--- src/screens/DestinationSearch/index.js
+++ src/screens/DestinationSearch/index.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
 import {SafeAreaView, Text, TextInput, View} from "react-native";
 import styles from "./styles";
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+
 
 const DestinationSearch = (props) => {
 
@@ -13,9 +15,24 @@ const DestinationSearch = (props) => {
             <View style={styles.container}>
                 <TextInput value={fromText} onChangeText={() => setFromText(text)} style={styles.textInput} placeholder="From" />
                 <TextInput value={destinationText} onChangeText={() => setDestinationText(text)} style={styles.textInput} placeholder="Where to" />
+
+                <GooglePlacesAutocomplete
+                    placeholder='Search'
+                    onPress={(data, details = null) => {
+                        // 'details' is provided when fetchDetails = true
+                        console.log(data, details);
+                    }}
+                    query={{
+                        key: 'AIzaSyBmXijpsVGRk39hnHdg6aWoeZ_Uaj81B-Y',
+                        language: 'en',
+                    }}
+                />
+
             </View>
         </SafeAreaView>
     );
 };
 
 export default DestinationSearch;
+
+/// AIzaSyBmXijpsVGRk39hnHdg6aWoeZ_Uaj81B-Y
