ea83ff4bd28a8255a2436a57fbd04c95cd3b15ea
Rokas <rookas.rudzenskas@gmail.com>
2021-07-28 18:04:05 +0300

    Rokas

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import HomeScreen from "./src/screens/HomeScreen";
+import DestinationSearch from "./src/screens/DestinationSearch";
 
 export default function App() {
   return (
       <SafeAreaView>
         <View style={styles.container}>
-          <HomeScreen />
+          {/*<HomeScreen />*/}
+            <DestinationSearch />
           <StatusBar style="auto" />
         </View>
       </SafeAreaView>
