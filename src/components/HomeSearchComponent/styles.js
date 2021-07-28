e3f6cdb08c38a40401e16fc60cbb6a25744983a8
Rokas <rookas.rudzenskas@gmail.com>
2021-07-28 17:15:02 +0300

    Rokas

--- src/components/HomeSearchComponent/styles.js
+++ src/components/HomeSearchComponent/styles.js
@@ -1,7 +1,21 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
+    inputBox: {
+        backgroundColor: '#b0b0b0',
+        margin: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
 
+    },
+
+    inputText: {
+
+    },
+    timeContainer: {
+
+    },
 });
 
 export default styles;
