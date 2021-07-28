9f03273479232e26ec76c2f08544fe7fc911c438
Rokas <rookas.rudzenskas@gmail.com>
2021-07-28 16:56:37 +0300

    Rakos

--- src/components/CovidMessage/styles.js
+++ src/components/CovidMessage/styles.js
@@ -2,7 +2,10 @@ import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
-
+        backgroundColor: "#2b80ff",
+        padding: 10,
+        borderTopLeftRadius: 10,
+        borderTopRightRadius: 10,
     },
     title: {
 
