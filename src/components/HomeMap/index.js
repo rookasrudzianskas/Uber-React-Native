0fa05c8f97ff29d8aa9c37376daaeae75e755c7e
Rokas <rookas.rudzenskas@gmail.com>
2021-07-29 10:56:23 +0300

    Rokas

--- src/components/HomeMap/index.js
+++ src/components/HomeMap/index.js
@@ -1,26 +1,24 @@
 import React from 'react';
-import {Text, View} from "react-native";
+import {Dimensions, StyleSheet, Text, View} from "react-native";
 import MapView from 'react-native-maps';
+import styles from "./styles";
 
 const HomeMap = () => {
     return (
         // <View style={{height: 300, alignItems: 'center', justifyContent: 'center'}}>
         //     <Text>I am the map</Text>
 
-        <View>
-            <MapView
-                style={{
-                    flex: 1
-                }}
-                initialRegion={{
-                    latitude: 37.78825,
-                    longitude: -122.4324,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421
-                }}
-            />
-        </View>
-        // </View>
+            <View style={styles.container}>
+                <MapView
+                    style={styles.map}
+                    initialRegion={{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421
+                    }}
+                />
+            </View>
     );
 };
 
