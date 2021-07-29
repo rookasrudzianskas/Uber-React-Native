ed3bbb1573096541d2a15d9d418c9ebe1903e573
Rokas <rookas.rudzenskas@gmail.com>
2021-07-29 09:29:49 +0300

    Rokas

--- src/components/UberTypeRow/index.js
+++ src/components/UberTypeRow/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Image, Text, View} from "react-native";
 import styles from "./styles";
+import {Ionicons} from "@expo/vector-icons";
 
 const UberTypeRow = () => {
     return (
@@ -8,7 +9,11 @@ const UberTypeRow = () => {
 
             <Image style={styles.image} source={require("../../../assets/images/UberX.jpeg")} />
             <View style={styles.middleContainer}>
+                <Text style={styles.type}>
+                    UberX
 
+                    <Ionicons name="person" size={24} color="black" />
+                </Text>
             </View>
 
             <View style={styles.rightContainer}>
