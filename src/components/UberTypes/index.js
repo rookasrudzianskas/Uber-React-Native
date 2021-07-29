ba45422a400ec95758c465869dce7e80bf41a791
Rokas <rookas.rudzenskas@gmail.com>
2021-07-29 10:17:49 +0300

    Rokas

--- src/components/UberTypes/index.js
+++ src/components/UberTypes/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {Text, View} from "react-native";
 import UberTypeRow from "../UberTypeRow";
 import typesData from "../../../assets/data/types";
+import {Ionicons, MaterialIcons} from "@expo/vector-icons";
+import styles from "./styles";
 
 const UberTypes = () => {
 
@@ -9,6 +11,27 @@ const UberTypes = () => {
     return (
         <View>
             {typesData.map(type => <UberTypeRow key={type.id} type={type} />)}
+
+
+            <View style={styles.container}>
+
+                <View style={styles.firstPart}>
+                    <View style={styles.rounded}>
+                        <Ionicons name="ios-person" size={24} color="white" />
+                    </View>
+                    <MaterialIcons name="party-mode" size={24} color="black" />
+
+                </View>
+
+                <View style={styles.card}>
+                    <Text styles={styles.text1}>Personal</Text>
+                    <Text styles={styles.text2}>Visa 0037</Text>
+                </View>
+
+                <View style={styles.arrow}>
+                    <Ionicons name="arrow-forward" size={24} color="black" />
+                </View>
+            </View>
         </View>
     );
 };
--- src/components/UberTypes/styles.js
+++ src/components/UberTypes/styles.js
@@ -1,7 +1,31 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginHorizontal: 20,
+    },
+    firstPart: {
+        flexDirection: 'row',
+        backgroundColor: 'lightgrey',
+        // padding: 10,
+        borderRadius: 30,
+        overflow: 'hidden',
+        alignItems: 'center',
+        justifyContent: 'center',
+        paddingRight: 15,
 
+    },
+    rounded: {
+        marginRight: 20,
+        backgroundColor: 'black',
+        width: 60,
+        height: 60,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderRadius: 30,
+    }
 });
 
 export default styles;
