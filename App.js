d5f597bb810709c727ef5b9c63a21fee4276bd2e
Rokas <rookas.rudzenskas@gmail.com>
2021-07-29 14:38:44 +0300

    Rokas

--- App.js
+++ App.js
@@ -1,13 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
 import HomeScreen from "./src/screens/HomeScreen";
 import DestinationSearch from "./src/screens/DestinationSearch";
 import SearchResults from "./src/screens/SearchResults";
 import HomeMap from "./src/components/HomeMap";
+import * as Location from "expo-location";
 
 export default function App() {
-  return (
+
+
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+
+    // live location things 👇
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation(location);
+        })();
+    }, []);
+
+    // end of live location thing 👆
+
+
+    return (
       // <SafeAreaView>
         <View style={styles.container}>
           {/*<HomeScreen />*/}
