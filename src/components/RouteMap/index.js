032b26ad81692cf9402bfabd0d0a6bc4e34ff1eb
Rokas <rookas.rudzenskas@gmail.com>
2021-07-29 11:48:38 +0300

    Rokas

--- src/components/RouteMap/index.js
+++ src/components/RouteMap/index.js
@@ -3,29 +3,24 @@ import {Dimensions, FlatList, Image, StyleSheet, Text, View} from "react-native"
 import MapView, {Marker} from 'react-native-maps';
 import styles from "./styles";
 import cars from "../../../assets/data/cars";
+import MapViewDirections from "react-native-maps-directions";
 
 const RouteMap = (props) => {
 
     const {type} = props;
-
-
-    const getImageName = (type) => {
-
-        if(type === 'UberX') {
-            return require('../../../assets/images/top-UberX.png');
-        }
-
-        if(type === 'Comfort') {
-            return require('../../../assets/images/top-Comfort.png');
-        }
-        return require('../../../assets/images/top-UberXL.png');
+    const origin = {
+        latitude: 28.450627,
+        longitude: -16.263045,
     }
 
-    return (
-        // <View style={{height: 300, alignItems: 'center', justifyContent: 'center'}}>
-        //     <Text>I am the map</Text>
+    const destination = {
+        latitude: 28.456208,
+        longitude: -16.259098,
+    }
 
-            // <View style={styles.container}>
+
+    return (
+
                 <MapView
                     style={{width: '100%', height: "100%"}}
                     provider="google"
@@ -38,20 +33,24 @@ const RouteMap = (props) => {
                         longitudeDelta: 0.0121,
                     }}
                 >
+                    <MapViewDirections
+                        origin={origin}
+                        destination={destination}
+                        apikey={"AIzaSyBmXijpsVGRk39hnHdg6aWoeZ_Uaj81B-Y"}
+                        strokeWidth={3}
+                        strokeColor="hotpink"
+                    />
 
-                    {cars.map((car) => (
 
-                        <Marker
-                            key={car.id}
-                            coordinate={{
-                            latitude: car.latitude,
-                            longitude: car.longitude }}
-                            // image={require("../../../assets/images/top-UberX.png")}
-                        >
-                            <Image source={getImageName(car.type)} style={{height: 60, width: 60, resizeMode: 'contain' }}/>
-                        </Marker>
+                        {/*<Marker*/}
+                        {/*    coordinate={{*/}
+                        {/*        latitude: 28.450627,*/}
+                        {/*        longitude: -16.263045, }}*/}
+                        {/*    // image={require("../../../assets/images/top-UberX.png")}*/}
+                        {/*>*/}
+                        {/*</Marker>*/}
 
-                    ))}
+
 
 
 
