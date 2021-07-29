c7691feaca89f7766fe5445901fc0dfe5cbafb7e
Rokas <rookas.rudzenskas@gmail.com>
2021-07-29 16:28:37 +0300

    Rokas

--- src/navigation/router.js
+++ src/navigation/router.js
@@ -3,6 +3,7 @@ import 'react-native-gesture-handler';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeNavigator from "./Home";
 import { createDrawerNavigator } from '@react-navigation/drawer';
+import {Text, View} from "react-native";
 
 
 const RouteNavigator = (props) => {
@@ -10,10 +11,16 @@ const RouteNavigator = (props) => {
     const Stack = createStackNavigator();
     const Drawer = createDrawerNavigator();
 
+    const something = (props) => {
+        <View style={{justifyContent: 'center', alignItems: 'center'}}>
+            <Text>{props.name}</Text>
+        </View>
+    }
+
 
     return (
 
-        <Drawer.Navigator>
+        <Drawer.Navigator initialRouteName="HomeNavigator">
 
             <Drawer.Screen name="Home" component={HomeNavigator} />
             {/*<Drawer.Screen name="Article" component={Article} />*/}
