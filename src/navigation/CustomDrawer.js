17a48e84c4567d14c8d87e9f67dec7fc41768a60
Rokas <rookas.rudzenskas@gmail.com>
2021-07-29 17:33:45 +0300

    Rokas

--- src/navigation/CustomDrawer.js
+++ src/navigation/CustomDrawer.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {Pressable, Text, View} from "react-native";
+import {Image, Pressable, Text, View} from "react-native";
 import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
-import {Entypo} from "@expo/vector-icons";
+import {Entypo, Foundation} from "@expo/vector-icons";
 
 
 const CustomDrawer = (props) => {
@@ -10,10 +10,22 @@ const CustomDrawer = (props) => {
 
             <View style={{backgroundColor: 'black', padding: 20,}}>
 
-                <View style={{marginBottom: 20,}}>
-                    <View style={{backgroundColor: '#cacaca', width: 60, height: 60, borderRadius: 30,}}>
+                <View style={{marginBottom: 10, marginTop: 20}}>
+
+                        <View style={{marginBottom: 20, flexDirection: 'row', alignItems: 'center'}}>
+                            <Image style={{width: 80, height: 80, borderRadius: 50,}} source={require("../../assets/images/rokas.jpeg")} />
+
+                            <View style={{marginLeft: 10, alignItems: 'flex-start'}}>
+                                <Text style={{color: 'white', fontSize: 18, marginBottom: 15}}>Rokas Rudzianskas</Text>
+                                <View style={{flexDirection: "row", marginHorizontal: 10, alignItems: 'center'}}>
+                                    <Text style={{color: "grey", fontSize: 18, marginRight: 10}}>5.00</Text>
+                                    <Foundation name="star" size={19} color="lightgrey" />
+                                </View>
+                            </View>
+                        {/*</View>*/}
 
                     </View>
+                    {/*</View>*/}
 
                 </View>
 
