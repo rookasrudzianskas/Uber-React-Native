95d6955af53114ab9683a694b5321583b5e6301a
Rokas <rookas.rudzenskas@gmail.com>
2021-07-29 09:20:57 +0300

    Roaks

---
+++ src/components/UberTypeRow/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+const UberTypeRow = () => {
+    return (
+        <View>
+            <Text>Uber Row</Text>
+        </View>
+    );
+};
+
+export default UberTypeRow ;
