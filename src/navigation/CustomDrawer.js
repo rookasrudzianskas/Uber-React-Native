f124fed568fb89d98cdfb2b9447018ad0b072f2c
Rokas <rookas.rudzenskas@gmail.com>
2021-07-29 16:53:34 +0300

    Rokas

--- src/navigation/CustomDrawer.js
+++ src/navigation/CustomDrawer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Pressable, Text, View} from "react-native";
 import { DrawerContentScrollView, DrawerItemList } from '@react-navigation/drawer';
+import {Entypo} from "@expo/vector-icons";
 
 
 const CustomDrawer = (props) => {
@@ -9,9 +10,12 @@ const CustomDrawer = (props) => {
 
             <View style={{backgroundColor: 'black', padding: 20,}}>
 
-                <View style={{borderTopWidth: 1, borderColor: '#eee', borderBottomWidth: 1}}>
+                <View style={{borderTopWidth: 1, borderColor: '#212121', borderBottomWidth: 1}}>
                     <Pressable onPress={() => console.log("Make Money Driving")}>
-                        <Text style={{color: '#dddddd', fontSize: 18, paddingVertical: 5,}}>Do more with your account</Text>
+                        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+                            <Text style={{color: '#dddddd', fontSize: 18, paddingVertical: 5,}}>Messages</Text>
+                            <Entypo style={{marginLeft: 10}}name="dot-single" size={27} color="Azure" />
+                        </View>
                     </Pressable>
                 </View>
 
