807edf0d3e280aab53ac6e1972aa0528e1dd8c3f
Rokas <rookas.rudzenskas@gmail.com>
2021-07-29 15:37:37 +0300

    Rokas

---
+++ src/navigation/router.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+
+const RouteNavigator = () => {
+    return (
+        <NavigationContainer>
+
+        </NavigationContainer>
+    );
+};
+
+export default RouteNavigator;
