fdbde4edfe02e6f75698797a96d40fb2736e9686
Rokas <rookas.rudzenskas@gmail.com>
2021-07-29 09:37:05 +0300

    Rokas

--- src/components/UberTypeRow/styles.js
+++ src/components/UberTypeRow/styles.js
@@ -2,10 +2,15 @@ import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
     container: {
-
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: 20,
     },
     image: {
-
+        height: 70,
+        width: 70,
+        resizeMode: 'contain',
     },
     middleContainer: {
 
