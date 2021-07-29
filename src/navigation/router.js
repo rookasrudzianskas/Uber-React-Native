441c9bb3ade3fb81f9eb98c1db91115e59268a3a
Rokas <rookas.rudzenskas@gmail.com>
2021-07-29 15:40:23 +0300

    Rokas

--- src/navigation/router.js
+++ src/navigation/router.js
@@ -2,11 +2,19 @@ import React from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import HomeScreen from "../screens/HomeScreen";
+import { createStackNavigator } from '@react-navigation/stack';
 
 const RouteNavigator = () => {
+
+    const Stack = createStackNavigator();
+
+
     return (
         <NavigationContainer>
-            <HomeScreen />
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen} />
+            </Stack.Navigator>
+
         </NavigationContainer>
     );
 };
