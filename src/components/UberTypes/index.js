2d18c575526f8b227969ca4ac5754bff8085741d
Rokas <rookas.rudzenskas@gmail.com>
2021-07-29 09:18:53 +0300

    Rokas

---
+++ src/components/UberTypes/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from "react-native";
+
+const UberTypes = () => {
+    return (
+        <View>
+            <Text>Done</Text>
+        </View>
+    );
+};
+
+export default UberTypes;
--- src/screens/SearchResults/index.js
+++ src/screens/SearchResults/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import {Text, View} from "react-native";
+import HomeMap from "../../components/HomeMap";
+import UberTypes from "../../components/UberTypes";
 
 const SearchResults = () => {
     return (
         <View>
-           <Text>Rokas</Text>
+           <HomeMap/>
+            <UberTypes />
         </View>
     );
 };
