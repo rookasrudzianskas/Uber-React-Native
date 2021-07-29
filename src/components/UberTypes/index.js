c527947309a0f66e432a1754df4b31984b66fe9e
Rokas <rookas.rudzenskas@gmail.com>
2021-07-29 09:21:42 +0300

    Roaks

--- src/components/UberTypes/index.js
+++ src/components/UberTypes/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {Text, View} from "react-native";
+import UberTypeRow from "../UberTypeRow";
 
 const UberTypes = () => {
     return (
         <View>
-            <Text>Done</Text>
+            <UberTypeRow />
         </View>
     );
 };
