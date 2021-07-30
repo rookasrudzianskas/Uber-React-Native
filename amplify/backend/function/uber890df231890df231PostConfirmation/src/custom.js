cbec2f4dfd7106b99863ee6e2a68160446c4045f
Rokas <rookas.rudzenskas@gmail.com>
2021-07-30 10:45:07 +0300

    Rokas

--- amplify/backend/function/uber890df231890df231PostConfirmation/src/custom.js
+++ amplify/backend/function/uber890df231890df231PostConfirmation/src/custom.js
@@ -4,6 +4,28 @@ const ddb = new aws.DynamoDB();
 
 exports.handler = async (event, context) => {
 
+    if(!event.request.userAttributes.sub) {
+        console.log("Error: No user was written to the DynamoDB");
+        // finish the function, done
+        context.done(null, event);
+        return;
+    }
+
+    // if we have user, we have to save to the Dynamo DB
+    const date = new Date();
+
+    const params = {
+        Item: {
+            'id': { S: event.request.userAttributes.sub},
+            // S is the string, what is value type
+            '__typename': { S: 'User' },
+            'username': { S: event.request.userAttributes.name },
+            'email':{ S: event.request.userAttributes.email },
+            'createdAt': { S: date.toISOString() },
+            'updatedAt': { S: date.toISOString() },
+        },
+        TableName: process.env.USERTABLE,
+    }
 }
 
 
